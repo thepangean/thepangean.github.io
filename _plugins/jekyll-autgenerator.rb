12c749d6d01324bd8ccd4f142d742fa05d7446a2
Kheeran Naidu <kn16063@my.bristol.ac.uk>
2019-01-03 14:51:24 +0000

    create profile even with 0 articles

--- _plugins/jekyll-autgenerator.rb
+++ _plugins/jekyll-autgenerator.rb
@@ -25,7 +25,7 @@ module Jekyll
 
     def paginate(site, type, posts)
       pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
-      (1..pages).each do |num_page|
+      (0..pages).each do |num_page|
         pager = Jekyll::Paginate::Pager.new(site, num_page, posts[1], pages)
         path = "/author/#{posts[0]}"
         if num_page > 1
@@ -34,7 +34,6 @@ module Jekyll
         newpage = GroupSubPageAuthor.new(site, site.source, path, type, posts[0])
         newpage.pager = pager
         site.pages << newpage
-
       end
     end
 
