907a5629656aff4c225deaa7f6fa6e809bae917d
Kheeran <kn16063@my.bristol.ac.uk>
2019-01-05 18:43:13 +0000

    fixing bug: post not on second author's feed

--- _plugins/jekyll-autgenerator.rb
+++ _plugins/jekyll-autgenerator.rb
@@ -40,8 +40,9 @@ module Jekyll
     private
 
     def posts_by_author(site, author)
-      site.posts.docs.select { |post| post.data['author'] == author }
+      site.posts.docs.select { |post| post.data['author'] == author || post.data['author1'] == author }
     end
+
   end
 
   class GroupSubPageAuthor < Page
