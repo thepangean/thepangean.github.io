6a85f34ebdf88b7fcc8e3a76effbd7a57fc54240
thepangean <utility@thepangean.com>
2021-06-14 10:04:41 +0100

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git checkout dev
 git  pull
+commit_id=$(git rev-parse HEAD)
 bundle install
 bundle exec jekyll build
 echo "BUILD COMPLETE!"
@@ -9,6 +10,7 @@ rm -r !("_site")
 mv _site/* ./
 rm -d _site
 git add .
-git commit -m "Deploy: $(date)"
+git commit -m "Deploy: $(date) $(commit_id)"
 git push
 git checkout dev
+echo "WEBSITE DEPLOYED!"
